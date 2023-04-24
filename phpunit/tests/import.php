d2b7fb96815e447cd9cb1fe7f58d7f97d9be6650
Grimace of Despair <GrimaceOfDespair@hotmail.com>
2023-04-24 02:59:11 +0000

    Add kyero unit test

--- phpunit/tests/import.php
+++ phpunit/tests/import.php
@@ -33,264 +33,42 @@ class Tests_Import_Import extends WP_Import_UnitTestCase {
 		parent::tear_down();
 	}
 
-	function test_small_import() {
+	function test_kyero_import() {
 		global $wpdb;
 
 		$authors = array(
 			'admin'  => false,
-			'editor' => false,
-			'author' => false,
 		);
-		$this->_import_wp( DIR_TESTDATA_KYERO_IMPORTER . '/small-export.xml', $authors );
-
-		// Ensure that authors were imported correctly.
-		$user_count = count_users();
-		$this->assertSame( 3, $user_count['total_users'] );
-		$admin = get_user_by( 'login', 'admin' );
-		$this->assertSame( 'admin', $admin->user_login );
-		$this->assertSame( 'local@host.null', $admin->user_email );
-		$editor = get_user_by( 'login', 'editor' );
-		$this->assertSame( 'editor', $editor->user_login );
-		$this->assertSame( 'editor@example.org', $editor->user_email );
-		$this->assertSame( 'FirstName', $editor->user_firstname );
-		$this->assertSame( 'LastName', $editor->user_lastname );
-		$author = get_user_by( 'login', 'author' );
-		$this->assertSame( 'author', $author->user_login );
-		$this->assertSame( 'author@example.org', $author->user_email );
-
-		// Check that terms were imported correctly.
-		$this->assertSame( '30', wp_count_terms( 'category' ) );
-		$this->assertSame( '3', wp_count_terms( 'post_tag' ) );
-		$foo = get_term_by( 'slug', 'foo', 'category' );
-		$this->assertSame( 0, $foo->parent );
-		$bar     = get_term_by( 'slug', 'bar', 'category' );
-		$foo_bar = get_term_by( 'slug', 'foo-bar', 'category' );
-		$this->assertSame( $bar->term_id, $foo_bar->parent );
+		$this->_import_wp( DIR_TESTDATA_KYERO_IMPORTER . '/kyero.xml', $authors );
 
 		// Check that posts/pages were imported correctly.
-		$post_count = wp_count_posts( 'post' );
-		$this->assertSame( '5', $post_count->publish );
-		$this->assertSame( '1', $post_count->private );
-		$page_count = wp_count_posts( 'page' );
-		$this->assertSame( '4', $page_count->publish );
-		$this->assertSame( '1', $page_count->draft );
-		$comment_count = wp_count_comments();
-		$this->assertSame( 1, $comment_count->total_comments );
+		$property_count = wp_count_posts( 'property' );
+		$this->assertSame( '2', $property_count->draft );
 
-		$posts = get_posts(
+		$properties = get_posts(
 			array(
-				'numberposts' => 20,
+				'numberposts' => 3,
 				'post_type'   => 'any',
 				'post_status' => 'any',
 				'orderby'     => 'ID',
 			)
 		);
-		$this->assertCount( 11, $posts );
+		$this->assertCount( 2, $properties );
 
-		$post = $posts[0];
-		$this->assertSame( 'Many Categories', $post->post_title );
-		$this->assertSame( 'many-categories', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'post', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$cats = wp_get_post_categories( $post->ID );
-		$this->assertCount( 27, $cats );
+		$property = $properties[0];
+		$this->assertSame( 'Apartment in Torre de la Horadada', $property->post_title );
+		$this->assertSame( 'apartment-in-torre-de-la-horadada', $property->post_name );
+		$this->assertSame( (string) $admin->ID, $property->post_author );
+		$this->assertSame( 'property', $property->post_type );
+		$this->assertSame( 'draft', $property->post_status );
+		$this->assertSame( 0, $property->post_parent );
 
-		$post = $posts[1];
-		$this->assertSame( 'Non-standard post format', $post->post_title );
-		$this->assertSame( 'non-standard-post-format', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'post', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$cats = wp_get_post_categories( $post->ID );
-		$this->assertCount( 1, $cats );
-		$this->assertTrue( has_post_format( 'aside', $post->ID ) );
-
-		$post = $posts[2];
-		$this->assertSame( 'Top-level Foo', $post->post_title );
-		$this->assertSame( 'top-level-foo', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'post', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$cats = wp_get_post_categories( $post->ID, array( 'fields' => 'all' ) );
-		$this->assertCount( 1, $cats );
-		$this->assertSame( 'foo', $cats[0]->slug );
-
-		$post = $posts[3];
-		$this->assertSame( 'Foo-child', $post->post_title );
-		$this->assertSame( 'foo-child', $post->post_name );
-		$this->assertSame( (string) $editor->ID, $post->post_author );
-		$this->assertSame( 'post', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$cats = wp_get_post_categories( $post->ID, array( 'fields' => 'all' ) );
-		$this->assertCount( 1, $cats );
-		$this->assertSame( 'foo-bar', $cats[0]->slug );
-
-		$post = $posts[4];
-		$this->assertSame( 'Private Post', $post->post_title );
-		$this->assertSame( 'private-post', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'post', $post->post_type );
-		$this->assertSame( 'private', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$cats = wp_get_post_categories( $post->ID );
-		$this->assertCount( 1, $cats );
-		$tags = wp_get_post_tags( $post->ID );
-		$this->assertCount( 3, $tags );
-		$this->assertSame( 'tag1', $tags[0]->slug );
-		$this->assertSame( 'tag2', $tags[1]->slug );
-		$this->assertSame( 'tag3', $tags[2]->slug );
-
-		$post = $posts[5];
-		$this->assertSame( '1-col page', $post->post_title );
-		$this->assertSame( '1-col-page', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'page', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$this->assertSame( 'onecolumn-page.php', get_post_meta( $post->ID, '_wp_page_template', true ) );
-
-		$post = $posts[6];
-		$this->assertSame( 'Draft Page', $post->post_title );
-		$this->assertSame( '', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'page', $post->post_type );
-		$this->assertSame( 'draft', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$this->assertSame( 'default', get_post_meta( $post->ID, '_wp_page_template', true ) );
-
-		$post = $posts[7];
-		$this->assertSame( 'Parent Page', $post->post_title );
-		$this->assertSame( 'parent-page', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'page', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$this->assertSame( 'default', get_post_meta( $post->ID, '_wp_page_template', true ) );
-
-		$post = $posts[8];
-		$this->assertSame( 'Child Page', $post->post_title );
-		$this->assertSame( 'child-page', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'page', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( $posts[7]->ID, $post->post_parent );
-		$this->assertSame( 'default', get_post_meta( $post->ID, '_wp_page_template', true ) );
-
-		$post = $posts[9];
-		$this->assertSame( 'Sample Page', $post->post_title );
-		$this->assertSame( 'sample-page', $post->post_name );
-		$this->assertSame( (string) $admin->ID, $post->post_author );
-		$this->assertSame( 'page', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$this->assertSame( 'default', get_post_meta( $post->ID, '_wp_page_template', true ) );
-
-		$post = $posts[10];
-		$this->assertSame( 'Hello world!', $post->post_title );
-		$this->assertSame( 'hello-world', $post->post_name );
-		$this->assertSame( (string) $author->ID, $post->post_author );
-		$this->assertSame( 'post', $post->post_type );
-		$this->assertSame( 'publish', $post->post_status );
-		$this->assertSame( 0, $post->post_parent );
-		$cats = wp_get_post_categories( $post->ID );
-		$this->assertCount( 1, $cats );
+		$property = $properties[1];
+		$this->assertSame( 'Commercial in Dehesa de Campoamor', $property->post_title );
+		$this->assertSame( 'commercial-in-dehesa-de-campoamor', $property->post_name );
+		$this->assertSame( (string) $admin->ID, $property->post_author );
+		$this->assertSame( 'property', $property->post_type );
+		$this->assertSame( 'draft', $property->post_status );
+		$this->assertSame( 0, $property->post_parent );
 	}
-
-	function test_double_import() {
-		$authors = array(
-			'admin'  => false,
-			'editor' => false,
-			'author' => false,
-		);
-		$this->_import_wp( DIR_TESTDATA_KYERO_IMPORTER . '/small-export.xml', $authors );
-		$this->_import_wp( DIR_TESTDATA_KYERO_IMPORTER . '/small-export.xml', $authors );
-
-		$user_count = count_users();
-		$this->assertSame( 3, $user_count['total_users'] );
-		$admin = get_user_by( 'login', 'admin' );
-		$this->assertSame( 'admin', $admin->user_login );
-		$this->assertSame( 'local@host.null', $admin->user_email );
-		$editor = get_user_by( 'login', 'editor' );
-		$this->assertSame( 'editor', $editor->user_login );
-		$this->assertSame( 'editor@example.org', $editor->user_email );
-		$this->assertSame( 'FirstName', $editor->user_firstname );
-		$this->assertSame( 'LastName', $editor->user_lastname );
-		$author = get_user_by( 'login', 'author' );
-		$this->assertSame( 'author', $author->user_login );
-		$this->assertSame( 'author@example.org', $author->user_email );
-
-		$this->assertSame( '30', wp_count_terms( 'category' ) );
-		$this->assertSame( '3', wp_count_terms( 'post_tag' ) );
-		$foo = get_term_by( 'slug', 'foo', 'category' );
-		$this->assertSame( 0, $foo->parent );
-		$bar     = get_term_by( 'slug', 'bar', 'category' );
-		$foo_bar = get_term_by( 'slug', 'foo-bar', 'category' );
-		$this->assertSame( $bar->term_id, $foo_bar->parent );
-
-		$post_count = wp_count_posts( 'post' );
-		$this->assertSame( '5', $post_count->publish );
-		$this->assertSame( '1', $post_count->private );
-		$page_count = wp_count_posts( 'page' );
-		$this->assertSame( '4', $page_count->publish );
-		$this->assertSame( '1', $page_count->draft );
-		$comment_count = wp_count_comments();
-		$this->assertSame( 1, $comment_count->total_comments );
-	}
-
-	/**
-	 * @ticket 21007
-	 */
-	public function test_slashes_should_not_be_stripped() {
-		global $wpdb;
-
-		$authors = array( 'admin' => false );
-		$this->_import_wp( DIR_TESTDATA_KYERO_IMPORTER . '/slashes.xml', $authors );
-
-		$alpha = get_term_by( 'slug', 'alpha', 'category' );
-		$this->assertSame( 'a \"great\" category', $alpha->name );
-
-		$tag1 = get_term_by( 'slug', 'tag1', 'post_tag' );
-		$this->assertSame( "foo\'bar", $tag1->name );
-
-		$posts = get_posts(
-			array(
-				'post_type'   => 'any',
-				'post_status' => 'any',
-			)
-		);
-		$this->assertNotEmpty( $posts );
-		$this->assertSame( 'Slashes aren\\\'t \"cool\"', $posts[0]->post_content );
-
-		$comments = get_comments(
-			array(
-				'post_id' => $posts[0]->post_ID,
-			)
-		);
-		$this->assertNotEmpty( $comments );
-		$this->assertSame( '\o/ ¯\_(ツ)_/¯', $comments[0]->comment_content );
-	}
-
-	/**
-	 * Ensure no PHP 8.1 deprecation notice is thrown when a URL is passed without a path component.
-	 *
-	 * Note: this test doesn't test anything else of the functionality in the `Kyero_Import::fetch_remote_file()` method!
-	 */
-	public function test_fetch_remote_file_php81_deprecation() {
-		$importer = new Kyero_Import();
-		$result   = $importer->fetch_remote_file( 'https://example.com', array() );
-
-		$this->assertWPError( $result, 'Call to fetch_remote_file() did not return expected WP Error object' );
-		$this->assertSame(
-			'Sorry, this file type is not permitted for security reasons.',
-			$result->get_error_message(),
-			'The WP Error object did not contain the expected error'
-		);
-	}
-
-	// function test_menu_import
 }
